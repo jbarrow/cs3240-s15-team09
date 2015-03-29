e7ac74b44e82716a686c043738f4cc0e2b01eced
Joe Barrow <joseph.d.barrow@gmail.com>
2015-03-29 11:49:18 -0400

    Changed from SQLite to MySQL

--- secure_witness/settings.py
+++ secure_witness/settings.py
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'secure_witness.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'secure_witness',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
