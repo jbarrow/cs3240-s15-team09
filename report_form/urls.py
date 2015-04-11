193ea6d477e7a3f1fc558641c8566ae0dd0c5418
Joe Barrow <joseph.d.barrow@gmail.com>
2015-04-11 16:09:54 -0400

    Can no suspend users and raise privileges

--- report_form/urls.py
+++ report_form/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 from report_form import views
 
-urlpatterns = patterns('', 
+urlpatterns = patterns('',
 url(r'^make-a-report/$', views.submission, name='submission'),
 url(r'^in-dev/$', views.incomplete_landing, name='incomplete_landing'),
 url(r'^submitted/$', views.submitted, name='submitted'),
