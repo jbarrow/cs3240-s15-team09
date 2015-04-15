46e8c176c11b909b5653c2a5c9a07dbc77a53d5f
Joe Barrow <joseph.d.barrow@gmail.com>
2015-04-14 20:01:47 -0400

    Now can authenticate from client

--- client/client.py
+++ client/client.py
@@ -1,11 +1,18 @@
 from getpass import getpass
 from optparse import OptionParser
+import requests, sys
 
 url = "http://localhost:8000/"
 
 def authenticate(username, password):
-    auth_url = url + "api/authenticate"
-    
+    auth_url = url + "api/authenticate/"
+    r = requests.post(auth_url, user)
+
+    if r.status_code == 200:
+        return r.json()["session"]
+
+    print("Something went wrong while authenticating")
+    sys.exit()
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -20,4 +27,4 @@ if __name__ == '__main__':
 
     password = getpass("Enter SecureWitness password: ")
 
-    authenticate(username, password)
+    session = authenticate(username, password)
