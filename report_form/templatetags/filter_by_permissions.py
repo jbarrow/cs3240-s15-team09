a4b60870a2014d893fda5b91f6d0921fe57f79ac
nnguyen <nicole.b.nguyen@gmail.com>
2015-04-21 18:39:06 -0400

    add more permissions stuff

--- report_form/templatetags/filter_by_permissions.py
+++ report_form/templatetags/filter_by_permissions.py
@@ -18,10 +18,13 @@ def filter_by_permissions(results):
 	profile = results.pop() # will remove and return the last item in the list
 	retSet = set()
 	for report in results:
-		p = get_object_or_404(Permission, report=report)
-		if profile.profile in p.profiles.all() or profile.profile == report.author:
+		if not report.private:
 			retSet.add(report)
-		for g in p.groups.all():
-			if profile in g.users.all():
+		else:
+			p = get_object_or_404(Permission, report=report)
+			if profile.profile in p.profiles.all() or profile.profile == report.author:
 				retSet.add(report)
+			for g in p.groups.all():
+				if profile in g.users.all():
+					retSet.add(report)
 	return retSet
