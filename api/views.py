69c8ed0fa54cf61ec880f475d2bdd99003d49767
Joe Barrow <joseph.d.barrow@gmail.com>
2015-04-22 22:07:31 -0400

    Updated API

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from django.utils.encoding import smart_str
 from django.contrib.auth.models import User
 from secure_witness.models import UserProfile
 from report_form.models import Report, File
+from report_form.filter_helper import filter_by_permissions
 
 import json, os
 from django.core import serializers
@@ -51,7 +52,7 @@ def get_reports(request):
         data = { "error": "User does not exist" }
         return HttpResponse(data)
 
-    reports = Report.objects.filter(author=user.profile)
+    reports = filter_by_permissions(Report.objects.all(), user)
 
     data = serializers.serialize("json", reports)
     return HttpResponse(data)
